a0e01f90c67bdbc90f4913ce0060f79f9c625789
Federico <fede.arribas@gmail.com>
2017-08-27 18:45:54 -0300

    Dev tool initial commit

---
+++ lib/farribas_view_tool.rb
@@ -0,0 +1,6 @@
+require "farribas_view_tool/version"
+require "farribas_view_tool/renderer"
+
+module FarribasViewTool
+  # Your code goes here...
+end
---
+++ lib/farribas_view_tool/version.rb
@@ -0,0 +1,3 @@
+module FarribasViewTool
+  VERSION = "0.1.0"
+end
